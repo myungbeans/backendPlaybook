ba74ba8b5addea39b518e5295eec8a443c618c3a
myungbeans <michael.h.myung@gmail.com>
2018-07-07 23:45:11 -0400

    players strong params permit moves attributes for updating

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,7 +1,7 @@
 require 'byebug'
 
 class Api::V1::PlayersController < ApplicationController
-    
+    before_action
     def create
         @player = Player.new
         @player.play_id = params[:player][:play_id]
@@ -16,12 +16,17 @@ class Api::V1::PlayersController < ApplicationController
 
     def update
         @player = Player.find(params[:id])
-
+        # @move = @player.moves.find{|move| move.id = params[:move_attributes][:id]}
         
-        if (@player.save)
+        if (@player.update(player_params))
             render json: { id: @player.id, name: @player.name, play_id: @player.play_id }
         else
             render json: { errors: @user.errors.full_messages}, status: :unprocessable_entity
         end
+    end
+
+    private
+    def player_params
+        params.require(:player).permit(:id, :name, moves_attributes: [:id, :player_id, :startX, :startY, :endX, :endY, :order, :startDelay, :endDelay, :duration])
     end
 end
