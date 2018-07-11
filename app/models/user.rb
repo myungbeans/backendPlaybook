93201375db0e7bd50640e0015eb23196db280e2e
myungbeans <michael.h.myung@gmail.com>
2018-07-11 13:20:50 -0400

    Added validations on Users model to check for email format and case insensitive username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     
     has_secure_password
 
-    validates :username, presence: true, uniqueness:true
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
     validates :password, presence: true
-    validates :email, presence: true, uniqueness:true
+    validates :email, presence: true, uniqueness: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
 end
